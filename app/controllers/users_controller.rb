81d29012307f3bf9fb92cd75e0aa2b899a82cafe
samcervantes <cervantes.samuel@gmail.com>
2020-04-10 00:34:38 -0600

    delete users#create controller action - the only correct way to create a user is with a devise registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_admin, except: [ :index, :show ]
+  before_action :require_admin, except: [ :index, :show, :edit ]
 
   # GET /users
   # GET /users.json
@@ -42,22 +42,6 @@ class UsersController < ApplicationController
       redirect_to root_url
     end
 
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1
