9742c839572529153afe6784603b86cf1b9a25cb
samcervantes <cervantes.samuel@gmail.com>
2020-04-09 22:31:44 -0600

    add user roles: unassigned, org_admin, admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :organization, optional: true
+
+  enum role: [:unassigned, :org_admin, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+  	self.role ||= :unassigned
+  end
+
 end
