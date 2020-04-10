9a7eca8e691a0d4657304588266deb55b8da42cb
samcervantes <cervantes.samuel@gmail.com>
2020-04-09 22:19:53 -0600

    update navbar to include sign_in / sign_out link

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  belongs_to :organization
+         :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :organization, optional: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'static_pages#index'
 
   get '/about', to: 'static_pages#about', as: 'about'
