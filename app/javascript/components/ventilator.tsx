daa4b3b62bd1b8fdb4b110c4974024e46d38b036
Patrick Holmes <patrick.j.holmes@gmail.com>
2020-04-17 23:32:55 -0700

    Implemented new API

--- app/javascript/components/ventilator.tsx
+++ app/javascript/components/ventilator.tsx
@@ -13,11 +13,12 @@ import React, { Component } from "react"
 import { get } from '../api'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+
 import {
   IVentilator,
-  IVentilatorPollValues,
-  IVentilatorPollResult,
-  IVentilatorApiCallResponse
+  IDevicePollResult,
+  IDeviceApiResponse,
+  IVentilatorApiV0CallResponse
 } from '../types'
 
 // When the device is connected, poll this often
@@ -27,24 +28,21 @@ const GOOD_POLL_PERIOD_MS = 3000
 const BAD_POLL_PERIOD_MS = 15000
 
 const API_KEY_HEADER = "Authorization"
-const API_KEY_PREFIX = 'OpenVentApiKeyV1' // followd by 1 space followed by apiKey
+const API_KEY_PREFIX = 'OpenVentApiKeyV1' // followed by 1 space followed by apiKey
 
 const VENTILATOR_NAME_WITH_SIMULATED_FAILURE = "East-2"
 
 interface IColumn {
   min: number,
-  max: number,
-  toDisplay: (x :number) => (number | string) // convert the measurement to what will be displayed
+  max: number
 }
 
-const identity = (x: number) : string => x.toString()
-
 const columns : {[key: string]: IColumn} = {
-   tidalVolume:             {min: 300, max: 800, toDisplay: identity},
-   respiratoryRate:         {min: 8,   max: 35,  toDisplay: identity},
-   peakInspiratoryPressure: {min: 60,  max: 80,  toDisplay: identity},
-   ieRatio:                 {min: 1,   max: 4,   toDisplay: x => `1:${x}`},
-   peep:                    {min: 5,   max: 10,  toDisplay: identity},
+   tidalVolume:             {min: 300, max: 800},
+   respiratoryRate:         {min: 8,   max: 35},
+   peakInspiratoryPressure: {min: 60,  max: 80},
+   peep:                    {min: 5,   max: 10},
+   ieRatio:                 {min: 1,   max: 4},
 }
 
 const columnNames = Object.keys(columns)
@@ -55,23 +53,22 @@ interface IProps {
 }
 
 interface IState {
-  pollResult: IVentilatorPollResult,
-  previousValues: IVentilatorPollValues,
+  pollResult: IDevicePollResult,
+  previousPollResult: IDevicePollResult,
 }
 
 class Ventilator extends Component<IProps, IState> {
   _timeout: any = null
   _mounted: boolean = false
+  // null is don't know yet, 0 is old api, 1 us new api - this is temporary (really!)
+  _apiVersion?: number = null
 
   constructor(props: IProps) {
     super(props)
 
     this.state = {
-      pollResult: {
-        connected: false,
-        result: null
-      },
-      previousValues: null
+      pollResult: null,
+      previousPollResult: null
     }
 
     this.poll = this.poll.bind(this)
@@ -83,8 +80,7 @@ class Ventilator extends Component<IProps, IState> {
     // if all ventilator objects are created at the same time and mount at the same time,
     // they will all update at the same time (every 3 seconds), causing all lines in the
     // table to change at the same time (and will all poll at the same time).
-    // This spaces them out. If we want them to update at the same time, just delete the
-    // call to delay as well as the delay function below.
+    // This spaces them out.
 
     // console.log(`${this.props.ventilator.name}: Mounted. Setting _mounted to true`)
     this._mounted = true
@@ -124,12 +120,11 @@ class Ventilator extends Component<IProps, IState> {
       return
     }
 
-    // copy the last values over to previousValues
-    // so we can detect changes and flash the fields that changed.
     this.setState({
       pollResult,
-      previousValues: {...this.state.pollResult.result}
+      previousPollResult: this.state.pollResult
     })
+
 
     var newPollingPeriod = pollResult.connected ? GOOD_POLL_PERIOD_MS : BAD_POLL_PERIOD_MS
 
@@ -138,87 +133,201 @@ class Ventilator extends Component<IProps, IState> {
     return
   }
 
-  async pollSimulatedDevice(): Promise<IVentilatorPollResult> {
-
+  async pollSimulatedDevice(): Promise<IDevicePollResult> {
     await delay(generateRandomValueBetween(0, 50))
 
     // If there is no result in state, then this is the first call, so return random values for all fields
-    if (! this.state.pollResult.result) {
-      let result = {
-        // for simulation purposes, a ventilator named 'EW Room2' will show as disconnected
-        connected: this.props.ventilator.name !== VENTILATOR_NAME_WITH_SIMULATED_FAILURE,
-        result: {
-          tidalVolume: generateRandomColumnValue('tidalVolume'),
-          respiratoryRate: generateRandomColumnValue('respiratoryRate'),
-          peakInspiratoryPressure: generateRandomColumnValue('peakInspiratoryPressure'),
-          ieRatio: generateRandomColumnValue('ieRatio'),
-          peep: generateRandomColumnValue('peep')
-        }
-      }
-      return result
+    if (! this.state.pollResult) {
+      return this.getRandomPollResult()
     }
+
+    let result = this.getFreshPollResultWithSameValues()
 
     // 50% of the time, don't change anything
     let change = generateRandomValueBetween(0, 1)
-
     if (change === 0) {
-      return this.state.pollResult
+      return result
     }
 
-    // Otherwise, pick one field to change, pick up or down, and adjust that field, ensuring it stays in range.
-    // To simplify, we will not change ieRatio. Just pick n-1 numbers and adjust
-    let columnIndx = generateRandomValueBetween(0, columnNames.length - 1)
-    let upDown = generateRandomValueBetween(0,1)
-
-    console.assert(columnIndx >= 0 && columnIndx < columnNames.length && upDown >= 0 && upDown <= 1,
-      `columnIndx ${columnIndx} upDown ${upDown}`)
-
-    let key = columnNames[columnIndx]
-    let value = this.state.pollResult.result[key]
-    value = upDown === 0 ? value + 1 : value - 1
-    value = clamp(value, columns[key].min, columns[key].max)
-
-    let pollResultValue = {...this.state.pollResult.result, [key]: value}
-
-    let result = {
-      connected: this.state.pollResult.connected,
-      result: pollResultValue
-    }
+    // otherwise, get a new status object with the value of one measuement item changed
+    this.updateOneMeasurementField(result)
 
     return result
   }
 
-  async pollDevice(): Promise<IVentilatorPollResult> {
+  getRandomPollResult(): IDevicePollResult {
+    let result = this.getPollResultTemplate()
+
+    result.connected = this.props.ventilator.name !== VENTILATOR_NAME_WITH_SIMULATED_FAILURE
+
+    let status = result.apiResponse.ventilatorDataMonitor.status
+    status.tidalVolume.value = generateRandomColumnValue('tidalVolume')
+    status.respiratoryRate.value = generateRandomColumnValue('respiratoryRate'),
+    status.peakInspiratoryPressure.value = generateRandomColumnValue('peakInspiratoryPressure'),
+    status.ieRatio.value = generateRandomColumnValue('ieRatio'),
+    status.peep.value = generateRandomColumnValue('peep')
+
+    return result
+  }
+
+  // get a PollResult with new timestamps, but copy over values
+  // for connected and ventilatorDataMonitor.status
+  getFreshPollResultWithSameValues(): IDevicePollResult {
+    let result = this.getPollResultTemplate()
+    result.connected = this.state.pollResult.connected
+
+    let fromStatus = this.state.pollResult.apiResponse.ventilatorDataMonitor.status
+    let toStatus = result.apiResponse.ventilatorDataMonitor.status
+
+    toStatus.tidalVolume.value = fromStatus.tidalVolume.value
+    toStatus.respiratoryRate.value = fromStatus.respiratoryRate.value
+    toStatus.peakInspiratoryPressure.value = fromStatus.peakInspiratoryPressure.value
+    toStatus.ieRatio.value = fromStatus.ieRatio.value
+    toStatus.peep.value = fromStatus.peep.value
+
+    return result
+  }
+
+  // Get a new response (with fresh timestamps), copy over the measurement from the current state, and then
+  // modify one of the measurement items. Pick one at random and pick the direction (+1 / -1) at random.
+  // ieRatio is a special case becasue the value is 1:n, so just mack it 1:2 or 1:4
+  updateOneMeasurementField(result: IDevicePollResult) : void {
+    let status = result.apiResponse.ventilatorDataMonitor.status
+    let columnIndx = generateRandomValueBetween(0, columnNames.length - 1)
+    let upDown = generateRandomValueBetween(0,1)
+    let key = columnNames[columnIndx]
+    let value = (key == "ieRatio") ? 3 : Number(status[key].value)
+    value = upDown === 0 ? value + 1 : value - 1
+    value = clamp(value, columns[key].min, columns[key].max)
+    let newValue = (key == "ieRatio") ? `1:${value}` : value.toString()
+    status[key].value = newValue
+    // console.log(`changed ${key} to ${newValue}`)
+  }
+
+  getPollResultTemplate(): IDevicePollResult {
+    let result: IDevicePollResult = {
+      connected: false,
+      apiResponse: {
+        device: {
+          id: this.props.ventilator.name,
+          currentTime: new Date(),
+          roles: {
+            ventilatorAlarmSoundMonitor: false,
+            ventilatorDataMonitor: true
+          }
+        },
+        ventilatorDataMonitor: {
+          timestamp: new Date(),
+          status: {
+            ieRatio: {
+              value: '',
+              uom: "ratio"
+            },
+            peakInspiratoryPressure: {
+              value: '',
+              uom: "CMH2O"
+            },
+            peep: {
+              value: '',
+              uom: "CMH2O"
+            },
+            respiratoryRate: {
+              value: '',
+              uom: "breathsPerMinute"
+            },
+            tidalVolume: {
+              value: '',
+              uom: "ml/kg"
+            }
+          },
+          alerts: {}
+        }
+      }
+    }
+    return result
+  }
+
+  async pollDevice(): Promise<IDevicePollResult> {
+    if (this._apiVersion === 1) {
+      return this.pollNewAPI()
+    }
+
+    if (this._apiVersion === 0) {
+      return this.pollOldAPI()
+    }
+
+    // we don't know which to use - try new then old
+    let result = await this.pollNewAPI()
+    if (result.connected) {
+      this._apiVersion = 1
+      return result
+    }
+
+    result = await this.pollOldAPI()
+    if (result.connected) {
+      this._apiVersion = 0
+      return result
+    }
+
+    // still don't know which to use, so we'll keep trying both until we do
+    return {
+      connected: false
+    }
+  }
+
+  // poll the old API and make the response look like an IDevicePollResult
+  async pollOldAPI(): Promise<IDevicePollResult> {
     let uri = `http://${this.props.ventilator.hostname}/api/ventilator`
 
-    // todo: check on why this returns an array
-    // console.log(`${this.props.ventilator.name}: Getting from: ${uri}`)
+    console.log(`${this.props.ventilator.name}: Getting from Old API at: ${uri}`)
 
     let headers = {}
     if (this.props.ventilator.apiKey) {
       headers[API_KEY_HEADER] = `${API_KEY_PREFIX} ${this.props.ventilator.apiKey.trim()}`
     }
 
-    let response = await get<IVentilatorApiCallResponse>(uri, headers)
-    // console.log(`${this.props.ventilator.name}: Response: ${JSON.stringify(response)}`)
+    let response = await get<IVentilatorApiV0CallResponse>(uri, headers)
+    console.log(`${this.props.ventilator.name}: Old API Response: ${JSON.stringify(response)}`)
 
     if (response.ok) {
-      // todo: validate the data within some range ?
+      let result = this.getPollResultTemplate()
+      result.connected = true
+      let status = result.apiResponse.ventilatorDataMonitor.status
       let v = response.parsedBody.ventilator[0]
-      const update : IVentilatorPollResult = {
-        connected: true,
-        result: {
-          tidalVolume: v.tidalVolume,
-          respiratoryRate: v.respiratoryRate,
-          peakInspiratoryPressure: v.peakInspiratoryPressure,
-          ieRatio: v.ieRatio,
-          peep: v.peep
-        }
-      }
-      return update
+      status.tidalVolume.value = v.tidalVolume.toString()
+      status.respiratoryRate.value = v.respiratoryRate.toString(),
+      status.peakInspiratoryPressure.value = v.peakInspiratoryPressure.toString(),
+      status.ieRatio.value = `1:${v.ieRatio}`,
+      status.peep.value = v.peep.toString()
+      return result
     }
 
-    return {connected: false, result: null}
+    return {connected: false}
+  }
+
+  async pollNewAPI(): Promise<IDevicePollResult> {
+    let uri = `http://${this.props.ventilator.hostname}/api/v1/status`
+
+    console.log(`${this.props.ventilator.name}: Getting from New API at: ${uri}`)
+
+    let headers = {}
+    if (this.props.ventilator.apiKey) {
+      headers[API_KEY_HEADER] = `${API_KEY_PREFIX} ${this.props.ventilator.apiKey.trim()}`
+    }
+
+    let response = await get<IDeviceApiResponse>(uri, headers)
+    console.log(`${this.props.ventilator.name}: Response: ${JSON.stringify(response)}`)
+
+    if (response.ok) {
+      // todo: validate response: schema, roles/keys, timestamps, UOMs, etc.
+      const result : IDevicePollResult = {
+        connected: true,
+        apiResponse: response.parsedBody
+      }
+      return result
+    }
+
+    return {connected: false}
   }
 
   render() {
@@ -231,21 +340,20 @@ class Ventilator extends Component<IProps, IState> {
     }
 
     const { ventilator } = this.props
-    const { pollResult, previousValues } = this.state
+    const { pollResult, previousPollResult } = this.state
 
-    let statusElement = pollResult.connected
+    let statusElement = pollResult?.connected
       ? <FontAwesomeIcon icon={faCircle} size="lg" color={'LimeGreen'}/>
       : <FontAwesomeIcon icon={faExclamationTriangle} size="lg" color={'red'} className="flash"/>
 
     const display = (columnName: string) : JSX.Element => {
-      if (! pollResult.connected) {
+      if (! pollResult?.connected) {
         return (<td>---</td>)
       }
-      let toDisplay = columns[columnName].toDisplay
-      let value = toDisplay(pollResult.result[columnName])
-      let changed = previousValues &&
-                    previousValues[columnName] &&
-                    (previousValues[columnName] !== pollResult.result[columnName])
+      let value = pollResult?.apiResponse?.ventilatorDataMonitor?.status[columnName]?.value
+      let preValue = previousPollResult?.apiResponse?.ventilatorDataMonitor?.status[columnName]?.value
+      let changed = previousPollResult?.connected && (value !== preValue)
+
       return changed
         ? <td className="pop">{value}</td>
         : <td>{value}</td>
@@ -275,13 +383,16 @@ const delay = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-const generateRandomColumnValue = (columnName: string) : any => {
+const generateRandomColumnValue = (columnName: string) : string => {
   let column = columns[columnName]
-  let value = generateRandomValueBetween(column.min, column.max)
+  let value = generateRandomValueBetween(column.min, column.max).toString()
+  if (columnName === 'ieRatio') {
+    value = `1:${value}`
+  }
   return value
 }
 
-const generateRandomValueBetween = (lower, upper) => {
+const generateRandomValueBetween = (lower: number, upper: number) : number => {
   return Math.round(Math.random()*(upper-lower)+lower)
 }
 
--- app/javascript/types.tsx
+++ app/javascript/types.tsx
@@ -18,24 +18,66 @@ export interface IVentilator {
   apiKey?: string
 }
 
-export interface IVentilatorPollValues {
-  tidalVolume: number
-  respiratoryRate: number
-  peakInspiratoryPressure: number
-  // todo: Technically, this is the denominator of the ratio
-  // The numerator is assumed to be 1. However, I think ratios
-  // can exceed one. Suggest we collect both numerator and denominator
-  ieRatio: number
-  peep: number
-}
+// Following types define the result of calling either /api/ventilator or /api/v1/device
 
-export interface IVentilatorApiCallResponse {
-  // todo: why is this an array? Can it ever have more than one value?
-  ventilator: IVentilatorPollValues[]
-}
-
-export interface IVentilatorPollResult {
+export interface IDevicePollResult {
   connected: boolean
-  result?: IVentilatorPollValues
+  apiResponse?: IDeviceApiResponse
+}
+
+export interface IDeviceApiResponse {
+  device: IDevice
+  ventilatorAlarmSoundMonitor?: IVentilatorAlarmSoundMonitor
+  ventilatorDataMonitor?: IVentilatorDataMonitor
+}
+
+export interface IDevice {
+  id: string
+  currentTime: Date
+  roles: {
+    ventilatorAlarmSoundMonitor: boolean
+    ventilatorDataMonitor: boolean
+  }
+}
+
+export interface IVentilatorAlarmSoundMonitor {
+  timestamp: Date
+  status: {}
+  alerts: {
+    audioAlarm: boolean
+  }
+}
+
+export interface IMeasurementValue {
+  value: string,
+  uom: string
+}
+
+export interface IVentilatorDataMonitor {
+  timestamp: Date
+  status: {
+    ieRatio: IMeasurementValue
+    peakInspiratoryPressure: IMeasurementValue
+    peep: IMeasurementValue
+    respiratoryRate: IMeasurementValue
+    tidalVolume: IMeasurementValue
+  }
+  alerts: {}
+}
+
+// These are only to support the "old" API
+
+
+export interface IVentilatorApiV0CallResponse {
+  // todo: why is this an array? Can it ever have more than one value?
+  ventilator: IVentilatorApiV0PollValues[]
+}
+
+export interface IVentilatorApiV0PollValues {
+  tidalVolume: string
+  respiratoryRate: string
+  peakInspiratoryPressure: string
+  ieRatio: string
+  peep: string
 }
 
